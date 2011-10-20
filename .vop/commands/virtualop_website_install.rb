d97983e895d916688fd1ba21706ce9efb75f4076
Philipp T <philipp@virtualop.org>
2011-10-20 17:54:44 +0200

    added install hook (untested)

---
+++ .vop/commands/virtualop_website_install.rb
@@ -0,0 +1,17 @@
+description 'installs the virtualop website project'
+
+param :machine
+param "domain", "the domain at which the website should be available", :mandatory => true
+
+on_machine do |machine, params|
+  machine.install_apache
+  machine.add_static_vhost("document_root" => "/var/www/virtualop_website", "server_name" => params["domain"])
+  machine.start_unix_service("name" => "httpd")
+
+  host_name = machine.name.split(".")[1..2].join(".")
+  proxy_name = "proxy." + host_name
+  @op.with_machine(proxy_name) do |proxy|
+    proxy.add_reverse_proxy("server_name" => [ params["domain"] ], "target_url" => "http://#{machine.ipaddress}/")
+    proxy.ssh_and_check_result("command" => "/etc/init.d/httpd restart")
+  end
+end
