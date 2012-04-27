82a31f6045225dcb268ce9551a879204cea963d7
Philipp T <philipp@virtualop.org>
2012-04-27 12:46:30 +0200

    installation fixes

--- .vop/commands/virtualop_website_install.rb
+++ .vop/commands/virtualop_website_install.rb
@@ -4,9 +4,16 @@ param :machine
 param "domain", "the domain at which the website should be available", :mandatory => true
 param "service_root", "fully qualified path to the location of the service", :mandatory => true
 
-on_machine do |machine, params|  
+on_machine do |machine, params|
+  dir_name = params["service_root"].split("/").last
+  new_root = "/var/www/#{dir_name}"
+  machine.ssh_and_check_result("command" => "mv #{params["service_root"]} #{new_root}")
+  # TODO probably only works on centos
+  machine.chown("file_name" => new_root, "ownership" => "apache")
+  
   machine.install_canned_service("service" => "apache/apache")
-  machine.add_static_vhost("document_root" => params["service_root"], "server_name" => params["domain"])
+  machine.add_static_vhost("document_root" => new_root, "server_name" => params["domain"])
+  
   machine.restart_unix_service("name" => "httpd")
   
   machine.configure_reverse_proxy("domain" => params["domain"])
