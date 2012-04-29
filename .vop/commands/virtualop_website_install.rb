7cc0029016d22c2d9a07dc41ad4a855d8c376a8a
Philipp T <philipp@virtualop.org>
2012-04-29 10:51:30 +0200

    ...

--- .vop/commands/virtualop_website_install.rb
+++ .vop/commands/virtualop_website_install.rb
@@ -8,7 +8,7 @@ on_machine do |machine, params|
   dir_name = params["service_root"].split("/").last
     
   new_root = "/var/www/#{dir_name}"
-  unless dir_name === new_root
+  unless params["service_root"] === new_root
     machine.ssh_and_check_result("command" => "mv #{params["service_root"]} #{new_root}")
     @op.without_cache do
       machine.list_working_copies
